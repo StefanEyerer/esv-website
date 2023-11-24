cad1cd7e27dae1ec04303b6cf97b65a94f077beb
Stefan Eyerer <stefan.eyerer@gmail.com>
2023-11-24 15:52:00 +0100

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./themes/esv-deggendorf/layouts/**/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      'avenir': ['"avenir next"', 'avenir', 'system-ui']
+    }
   },
   plugins: [],
 }
