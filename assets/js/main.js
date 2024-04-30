98a37172740310d8fc265dc261c60b52a5a3f040
Stefan Eyerer <stefan.eyerer@gmail.com>
2024-04-30 15:00:29 +0200

    Improve menu, header and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("site built with hugo");
+window.addEventListener('load', () => {
+    const mainNav = document.querySelector("#main-nav");
+    const menuBtn = document.querySelector("#menu-btn");
+    
+    menuBtn.addEventListener('click', () => {
+        if (mainNav.getAttribute('data-menu-open') === 'true') {
+            mainNav.setAttribute('data-menu-open', 'false');
+            menuBtn.setAttribute('data-menu-open', 'false');
+        } else {
+            mainNav.setAttribute('data-menu-open', 'true');
+            menuBtn.setAttribute('data-menu-open', 'true');
+        }
+    });
+});
